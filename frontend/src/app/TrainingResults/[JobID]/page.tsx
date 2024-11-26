657110bfcbbdd8ca5e91d896af2c45aa88029416
Mohamed Shakir <mshak084@uottawa.ca>
2024-11-26 00:38:10 -0500

    Fixing props for async page

--- frontend/src/app/TrainingResults/[JobID]/page.tsx
+++ frontend/src/app/TrainingResults/[JobID]/page.tsx
@@ -1,12 +1,11 @@
 import Dashboard from "@/app/TrainingResults/_components/Dashboard";
 import { trainModel, getJobById, initializeJob } from "@/lib/lib";
 
-export default async function OverViewPage({
-	params,
-}: {
-	params: Promise<{ jobID: string }>;
-}) {
-	const jobID = (await params).jobID;
+type Params = Promise<{ jobID: string }>;
+
+export default async function OverViewPage(props: { params: Params }) {
+	const params = await props.params;
+	const jobID = params.jobID;
 	let job = await getJobById(jobID);
 	if (!job) {
 		job = await initializeJob(jobID);
